e37110d6ead61f28f4ad6d8853054d1cac3422eb
Seth Fitzsimmons <seth@mojodna.net>
2017-05-18 17:43:02 -0700

    Survey compilation
    Incorporates functions to be scrounged when assembling survey
    definitions for display and data entry.
    
    Sample output (from sri_lanka.yaml):
      https://gist.github.com/mojodna/8bbaeb0b286394a4e55a519f1dd9b281

---
+++ tools/README.md
@@ -0,0 +1,9 @@
+# Surveyor Tools
+
+This contains some supporting tools and experiments for managing
+Surveyor-related data.
+
+## `compile-survey`
+
+Compiles YAML survey definitions and loads associated presets, fields, and
+options.
---
+++ tools/bin/compile-survey.js
@@ -0,0 +1,292 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const async = require("async");
+const uniqBy = require("lodash.uniqby");
+const yaml = require("js-yaml");
+
+const argv = process.argv.slice(2);
+
+if (argv.length === 0) {
+  console.error("Usage: compile-survey /path/to/survey.yaml");
+  process.exit(1);
+}
+
+const surveyConfig = path.resolve(argv.shift());
+const BASE_PATH = path.dirname(surveyConfig);
+const PRESET_PATH = path.resolve(BASE_PATH, "..", "presets", "presets");
+const FIELD_PATH = path.resolve(BASE_PATH, "..", "presets", "fields");
+const OPTION_PATH = path.resolve(BASE_PATH, "..", "presets", "options");
+
+let surveyDefinition;
+
+try {
+  surveyDefinition = yaml.safeLoad(fs.readFileSync(surveyConfig));
+} catch (err) {
+  throw err;
+}
+
+const getParser = type => {
+  switch (type.toLowerCase()) {
+    case "json":
+      return JSON.parse;
+
+    case "yml":
+    case "yaml":
+      return yaml.safeLoad;
+
+    default:
+      throw new Exception(`Unsupported type: ${type}`);
+  }
+};
+
+const parse = (path, type, callback) => {
+  return fs.readFile(path, (err, body) => {
+    if (err) {
+      return callback(err);
+    }
+
+    try {
+      return callback(null, getParser(type)(body));
+    } catch (err) {
+      console.warn(err.stack);
+      return callback(err);
+    }
+  });
+};
+
+const loadField = (fieldName, callback) => {
+  // TODO cache
+  return parse(path.resolve(FIELD_PATH, fieldName + ".json"), "json", callback);
+};
+
+const loadPreset = (presetName, callback) => {
+  // TODO cache
+  return parse(
+    path.resolve(PRESET_PATH, presetName + ".json"),
+    "json",
+    callback
+  );
+};
+
+const resolvePreset = (preset, callback) => {
+  return async.map(preset.fields, loadField, (err, fields) => {
+    if (err) {
+      return callback(err);
+    }
+
+    return callback(
+      null,
+      Object.assign(preset, {
+        fields
+      })
+    );
+  });
+};
+
+const resolveField = (field, callback) => {
+  if (typeof field === "string") {
+    return loadField(field.replace(/:/g, "/"), callback);
+  }
+
+  return loadField(field.type.replace(/:/g, "/"), (err, fieldDefn) => {
+    if (err) {
+      return callback(err);
+    }
+
+    if (field.options != null) {
+      fieldDefn.strings = fieldDefn.strings || {};
+      fieldDefn.strings.options = field.options;
+    }
+
+    return callback(null, fieldDefn);
+  });
+};
+
+const resolveFields = (fields, callback) => {
+  if (fields != null) {
+    return async.map(fields, resolveField, callback);
+  }
+
+  return setImmediate(callback, null, []);
+};
+
+const resolveFeatureType = (featureType, callback) => {
+  // console.warn("featureType:", featureType);
+  const { key, value } = featureType;
+
+  const presetName = [key, value].filter(x => x != null).join("/");
+
+  return loadPreset(presetName, (err, preset) => {
+    if (err) {
+      return callback(err);
+    }
+
+    const { exclude } = featureType;
+
+    // remove fields that should be excluded
+    if (exclude != null) {
+      preset.fields = preset.fields.filter(field => !exclude.includes(field));
+    }
+
+    return resolvePreset(preset, (err, preset) => {
+      if (err) {
+        return callback(err);
+      }
+
+      const { extend, id, include, name, options } = featureType;
+
+      // overrides
+      preset.id = id;
+      preset.name = name || preset.name;
+
+      // stash extension info for post-processing
+      if (extend != null) {
+        preset.extend = extend;
+      }
+
+      // append custom fields
+      return resolveFields(include, (err, fields) => {
+        if (err) {
+          return callback(err);
+        }
+
+        preset.fields.concat(fields);
+
+        // populate custom options
+        if (options != null) {
+          Object.keys(options).forEach(key => {
+            const opts = options[key];
+
+            const field = preset.fields.find(f => f.key === key);
+
+            if (field != null) {
+              field.strings = field.strings || {};
+              field.strings.options = opts;
+            }
+          });
+        }
+
+        return callback(null, preset);
+      });
+    });
+  });
+};
+
+const resolveFeatureTypes = (featureTypes, callback) => {
+  return async.map(featureTypes, resolveFeatureType, callback);
+};
+
+const resolveSurvey = (surveyDefinition, callback) => {
+  const { feature_types } = surveyDefinition;
+
+  return resolveFeatureTypes(feature_types, (err, featureTypes) => {
+    if (err) {
+      return callback(err);
+    }
+
+    featureTypes = featureTypes.map(ft => {
+      if (ft.extend != null) {
+        // the original definition of this feature type
+        const ftDef = feature_types.find(x => x.id == ft.id);
+        // definitions to extend
+        const defs = [];
+
+        if (Array.isArray(ft.extend)) {
+          defs.concat(feature_types.filter(x => ft.extend.includes(x.id)));
+        } else {
+          defs.push(feature_types.find(x => x.id == ft.extend));
+        }
+
+        let localFields = ft.fields;
+        let inheritedFields = [];
+
+        defs.forEach(def => {
+          if (def.exclude != null) {
+            let toRemove = [];
+
+            if (ftDef.include != null) {
+              // resolve the list of included fields to their type
+              const included = ftDef.include.map(i => i.type || i);
+
+              // locally included fields should override what the parent excludes
+              toRemove = toRemove.concat(
+                def.exclude.filter(e => !included.includes(e))
+              );
+            } else {
+              toRemove = toRemove.concat(def.exclude);
+            }
+
+            // remove excluded fields
+            localFields = localFields.filter(f => !toRemove.includes(f.type));
+          }
+
+          // concat inherited fields
+          const superType = featureTypes.find(x => x.id == def.id);
+          inheritedFields = inheritedFields.concat(superType.fields);
+        });
+
+        // append + dedupe local fields (both custom + from presets)
+        ft.fields = uniqBy(inheritedFields.concat(localFields), "key");
+
+        // clean up after ourselves
+        delete ft.extend;
+      }
+
+      return ft;
+    });
+
+    const { observation_types } = surveyDefinition;
+
+    const observationTypes = featureTypes.filter(type =>
+      observation_types.includes(type.id)
+    );
+
+    const {
+      attachments,
+      description,
+      imagery,
+      meta,
+      name,
+      sync,
+      version
+    } = surveyDefinition;
+    let { anonymous, editable } = surveyDefinition;
+
+    // defaults
+    if (anonymous == null) {
+      anonymous = true;
+    }
+
+    if (editable == null) {
+      editable = true;
+    }
+
+    const survey = {
+      anonymous,
+      attachments,
+      description,
+      editable,
+      imagery,
+      meta,
+      name,
+      sync,
+      version,
+      featureTypes,
+      observationTypes
+    };
+
+    return callback(null, survey);
+  });
+};
+
+resolveSurvey(surveyDefinition, (err, survey) => {
+  if (err) {
+    throw err;
+  }
+
+  process.stdout.write(JSON.stringify(survey));
+});
