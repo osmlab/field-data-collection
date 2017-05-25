42a406bd68cf4b408e7b57d5d9d6df5f8f02f86b
Seth Fitzsimmons <seth@mojodna.net>
2017-05-25 15:58:46 -0700

    Fix back arrow

--- screens/observation-create/add-observation.js
+++ screens/observation-create/add-observation.js
@@ -13,19 +13,16 @@ class AddObservationScreen extends React.Component {
     }
   }
 
+  onBackPress = () => this.props.navigation.dispatch(NavigationActions.back());
+
   render () {
     const { navigate } = this.props.navigation;
     const { category } = this.state;
 
-    function onBackPress () {
-      const backAction = NavigationActions.back()
-      this.props.navigation.dispatch(backAction)
-    }
-
     return (
       <View style={baseStyles.container}>
         <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
-          <Text style={{fontSize:30, marginTop: -10, marginRight: 5}} onPress={onBackPress}>←</Text>
+          <Text style={{fontSize:30, marginTop: -10, marginRight: 5}} onPress={this.onBackPress}>←</Text>
           <Text style={[baseStyles.title]}>Adding: {category.name}</Text>
         </View>
 
