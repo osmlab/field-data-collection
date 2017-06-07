8dfff56677696ce4525991c39ea000882797d7f8
sethvincent <sethvincent@gmail.com>
2017-06-07 18:23:47 -0400

    use react-native-material-kit

--- android/app/src/main/java/com/surveyor/MainApplication.java
+++ android/app/src/main/java/com/surveyor/MainApplication.java
@@ -27,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
         new ReactNativeMapboxGLPackage(),
-        new ReactNativeLocalizationPackage()
+        new ReactNativeLocalizationPackage(),
+        new ReactMaterialKitPackage()
       );
     }
   };
