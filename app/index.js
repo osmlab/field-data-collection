31421e645fbfd3d20ce86ca4be14c0673b2ffe48
sethvincent <sethvincent@gmail.com>
2017-06-15 20:22:18 -0400

    move and improve osm-p2p, remove temporary changes

--- app/index.js
+++ app/index.js
@@ -6,14 +6,11 @@ import { Provider } from "react-redux";
 import store from "./config/store";
 import App from "./screens/App";
 
-// temporary, for testing websockets:
-import AddSurveyScreen from "./screens/Account/add-survey";
-
 class Root extends Component {
   render() {
     return (
       <Provider store={store}>
-        <AddSurveyScreen />
+        <App />
       </Provider>
     );
   }
--- app/screens/Account/add-survey.js
+++ app/screens/Account/add-survey.js
@@ -4,25 +4,6 @@ import { View, Button } from "react-native";
 import { Text, Wrapper } from "../../components";
 import { baseStyles } from "../../styles";
 
-import websocket from "websocket-stream";
-
-import hyperlog from "hyperlog";
-const levelup = require("levelup");
-const asyncstorage = require("asyncstorage-down");
-
-import osmp2p from "../../../osm-p2p";
-var osm = osmp2p();
-
-osm.create({ type: "node", lat: 64.6, lon: -147.8 }, (err, key, node) => {
-  if (err) return console.error("error", err);
-  console.log("created key", key, "node", node);
-
-  osm.query([[64, 65], [-148, -147]], function(err, pts) {
-    if (err) console.error(err);
-    console.log("pts", pts);
-  });
-});
-
 class AddSurveyScreen extends Component {
   render() {
     return (
@@ -35,17 +16,6 @@ class AddSurveyScreen extends Component {
           <Text style={baseStyles.title}>
             Add a survey
           </Text>
-
-          <Button
-            title="Add survey"
-            onPress={() => {
-              console.log("sync started");
-              const ws = websocket("ws://10.0.2.2:3131");
-              var stream = osm.log.replicate();
-              stream.on("error", console.log);
-              stream.pipe(ws).pipe(stream);
-            }}
-          />
         </View>
       </Wrapper>
     );
