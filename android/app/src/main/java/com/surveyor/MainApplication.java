ee9de24498e3c854752212d8a78dedea4d294e1a
sethvincent <sethvincent@gmail.com>
2017-06-26 17:43:26 -0400

    wip pie charts

--- android/app/src/main/java/com/surveyor/MainApplication.java
+++ android/app/src/main/java/com/surveyor/MainApplication.java
@@ -3,6 +3,7 @@ package com.surveyor;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.tradle.react.UdpSocketsModule;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new SvgPackage(),
         new Interactable(),
         new UdpSocketsModule(),
         new ReactNativeMapboxGLPackage(),
--- app/components/percent-complete.js
+++ app/components/percent-complete.js
@@ -4,31 +4,100 @@ import { View, TouchableHighlight, TouchableOpacity } from "react-native";
 import Text from "./text";
 import { colors } from "../styles";
 import { baseStyles } from "../styles";
-import PieChart from "react-native-pie-chart";
+import { Pie } from "react-native-pathjs-charts";
+import Svg, { G, Path, Circle } from "react-native-svg";
+var Pie = require("paths-js/pie");
 
 class PercentComplete extends Component {
   setNativeProps(nativeProps) {
     this._root.setNativeProps(nativeProps);
   }
   render() {
+    let data = [
+      {
+        name: "complete",
+        amount: 5
+      },
+      {
+        name: "incomplete",
+        amount: 3
+      }
+    ];
+
+    let options = {
+      margin: {
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
+      },
+      width: 50,
+      height: 50,
+      color: "#2980B9",
+      r: 15,
+      R: 25,
+      legendPosition: false,
+      label: false
+    };
+
     return (
-      <View style={[baseStyles.percentCompleteWrapper]}>
-        <View style={[baseStyles.percentComplete]}>
-          <PieChart
-            style={{ alignSelf: "center" }}
-            chart_wh={50}
-            series={[80]}
-            sliceColor={["#1DE9B6"]}
-            doughnut={true}
-            coverRadius={0.7}
-            coverFill={"#FFF"}
-          />
+      <View style={[]}>
+        <View
+          style={[
+            {
+              flex: 1,
+              alignItems: "center"
+            }
+          ]}
+        >
+          <Pie data={data} options={options} accessorKey="amount" />
           <Text style={[baseStyles.percentCompleteTextSm]}>
             <Text style={[baseStyles.percentCompleteTextNumSm]}>80%</Text>
           </Text>
         </View>
       </View>
     );
+  }
+}
+
+class PieChart extends Component {
+  render() {
+    let chart = Pie({
+      center: [centerX, centerY],
+      r,
+      R,
+      data: this.props.data,
+      accessor: this.props.accessor || identity(this.props.accessorKey)
+    });
+
+    slices = chart.curves.map((c, i) => {
+      let fill = (c.item.color && Colors.string(c.item.color)) || this.color(i);
+      let stroke = typeof fill === "string" ? fill : Colors.darkenColor(fill);
+      return (
+        <G key={i}>
+          <Path
+            d={c.sector.path.print()}
+            stroke={stroke}
+            fill={fill}
+            fillOpacity={1}
+          />
+          <G x={options.margin.left} y={options.margin.top}>
+            <Text
+              fontFamily={textStyle.fontFamily}
+              fontSize={textStyle.fontSize}
+              fontWeight={textStyle.fontWeight}
+              fontStyle={textStyle.fontStyle}
+              fill={textStyle.fill}
+              textAnchor="middle"
+              x={c.sector.centroid[0]}
+              y={c.sector.centroid[1]}
+            >
+              {c.item.name}
+            </Text>
+          </G>
+        </G>
+      );
+    });
   }
 }
 
