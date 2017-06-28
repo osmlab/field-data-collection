fb1cf73bb84b79344cc721d0d191207a449b3a9c
Stephen Whitmore <sww@eight45.net>
2017-06-28 12:30:35 -0700

    Fix generate-id to start with hyphen; run prettier.

--- app/lib/generate-id.js
+++ app/lib/generate-id.js
@@ -1,7 +1,14 @@
-var _counter = 0
+var _counter = 0;
 
 function generatePlaceholderOsmId() {
-  return Math.random().toString().substring(2) + '-' + Date.now() + '-' + _counter++
+  return (
+    "-" +
+    Math.random().toString().substring(2) +
+    "-" +
+    Date.now() +
+    "-" +
+    _counter++
+  );
 }
 
 module.exports = generatePlaceholderOsmId;
