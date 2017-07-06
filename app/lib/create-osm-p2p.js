58f1d54ea182e1173506f24d82df16f872ac3142
Stephen Whitmore <sww@eight45.net>
2017-07-06 13:02:15 -0700

    Implement AsyncStorage-aware "db.clear()" method.

--- app/lib/create-osm-p2p.js
+++ app/lib/create-osm-p2p.js
@@ -9,11 +9,24 @@ function createOsmDb(prefix) {
   const logdb = levelup(prefix + "-db", { db: asyncstorage });
   const log = hyperlog(logdb, { valueEncoding: "json" });
 
-  return osmdb({
+  var db = osmdb({
     log: log,
     db: levelup(prefix + "-index", { db: asyncstorage }),
     store: createStore(1024, prefix + "-chunks")
   });
+
+  db.clear = function(cb) {
+    var pending = 3;
+    asyncstorage.destroy(prefix + "-db", onDone);
+    asyncstorage.destroy(prefix + "-index", onDone);
+    store.destroy(onDone);
+
+    function onDone() {
+      if (--pending === 0) db();
+    }
+  };
+
+  return db;
 }
 
 module.exports = createOsmDb;
