2c81b2e6b5fab43e0c1bcbd3c8146d608eeaa9bb
sethvincent <sethvincent@gmail.com>
2017-09-22 17:21:00 -0400

    wait until we have users current position to update bounds

--- app/screens/Observation/map.js
+++ app/screens/Observation/map.js
@@ -149,10 +149,9 @@ class ObservationMapScreen extends Component {
     getCurrentPosition((err, data) => {
       if (data) {
         this.setState({ userLocation: data.coords });
+        this.onUpdateBounds();
       }
     });
-
-    this.onUpdateBounds();
   };
 
   onRegionDidChange = info => this.onUpdateBounds();
@@ -176,9 +175,9 @@ class ObservationMapScreen extends Component {
   onUpdateBounds = () => {
     const { updateVisibleBounds } = this.props;
 
-    // NOTE getBounds returns (lat, lon, lat, lon) so we convert it here
     if (this._map) {
       this._map.getBounds(bounds =>
+        // NOTE getBounds returns (lat, lon, lat, lon) so we convert it here
         updateVisibleBounds([bounds[1], bounds[0], bounds[3], bounds[2]])
       );
     }
